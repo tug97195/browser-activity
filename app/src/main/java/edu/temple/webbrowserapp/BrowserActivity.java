70c98f98f17708547d08315ddcc8bdf791dd9adf
tug97195 <60144154+tug97195@users.noreply.github.com>
2020-12-07 18:07:15 -0500

    Update BrowserActivity.java

--- app/src/main/java/edu/temple/webbrowserapp/BrowserActivity.java
+++ app/src/main/java/edu/temple/webbrowserapp/BrowserActivity.java
@@ -5,7 +5,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
